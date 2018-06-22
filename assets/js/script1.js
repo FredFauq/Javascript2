062a9280c42297225c7bc600f2dada72d601cb42
FredericF <frederic.fauquembert@sfr.fr>
2018-06-22 12:06:53 +0200

    Exercice 1

---
+++ assets/js/script1.js
@@ -0,0 +1,7 @@
+function mouseOver() {
+    document.getElementById('image1').src='assets/img/image1_2.jpg';
+}
+
+function mouseOut() {
+    document.getElementById('image1').src='assets/img/image1.jpg';
+}
